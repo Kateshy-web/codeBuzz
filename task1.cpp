74adef528b5adbe278e09b9676846b4566e93606
Kateshy-web <Kateshy-web>
2023-09-23 13:47:09 +0530

    string concatenation

---
+++ task1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string name;
+    string surname;
+    int age;
+
+    cout<<"Enter your first name: ";
+    cin>>name;
+
+    cout<<"Enter your last name: ";
+    cin>>surname;
+
+    cout<<"Enter your age: ";
+    cin>>age;
+
+    cout<<"\nThe user full name is "<<name<<" "<<surname<<" and he is "<<age<<" years old."<<endl;
+
+}
